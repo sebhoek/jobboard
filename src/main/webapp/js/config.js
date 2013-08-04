f92d7672b81c5f18ee07d05cf09a007f3dd50bd4
sebhoek <sebastian.haehnel@gmail.com>
2013-08-04 17:42:29 +0200

    use jenkins url

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -1,3 +1,3 @@
-var jenkinsBaseUrl = 'jenkins';
-var displayedTimeInMillies = 1000 * 60 * 60 * 12; // 12 hrs;
-//var jenkinsBaseUrl = 'http://localhost:8080';
+//var jenkinsBaseUrl = 'jenkins';
+var displayedTimeInMillies = 1000 * 60 * 60 * 2; // 12 hrs;
+var jenkinsBaseUrl = 'http://localhost:8080';
